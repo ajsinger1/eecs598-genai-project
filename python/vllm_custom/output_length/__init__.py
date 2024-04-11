5b5527ff669d45ea8eb9516e5d29bf7c7b9ead1e
Ari Singer <ajsinger1@gmail.com>
2024-04-11 15:09:01 -0400

    Fix prompts with output path and delete build

--- python/vllm_custom/output_length/__init__.py
+++ python/vllm_custom/output_length/__init__.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from llama_chat_helpers import format_prompt
-prompts_with_output_path = '/content/drive/MyDrive/Systems for Gen AI W24/colab/data/prompts-with-output.json'
+prompts_with_output_path = '/content/drive/MyDrive/eecs598_genai/github_repo/data/prompts-with-output.json'
 data = pd.read_json(prompts_with_output_path)
 output_lengths = {format_prompt(data["prompt"][i]): float(data["output_length"][i]) for i in data.index}
 print("\033[1;31;40m Using custom vLLM implementation with output length priority function")
